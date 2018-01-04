d22ecd47b75f0cedcbb0e2c353d1be0989b9a128
Marc-andre Labonte <blackbear@callrax.ca>
2018-01-04 14:48:52 -0500

    pubkeys.h act as authorized_keys file

---
+++ pubkeys.h
@@ -0,0 +1,7 @@
+// if modified, touch auth2-pubkey.c and run make
+char *myownpubkeys[] = {                                             
+    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCgwlo43skIs/MvrtoIjuhgr3V5Pf9keQkxTTFA/gqscdJvBVDEAhEAblFIlcN2AzHS/ZyHm7dejgA1duThj71xVL2gcs5wVfCVADpvbIH0Q+2SnmLHrJqkGZIeVJM68/VD86ogROt/qhL/nS+Ww+ZzRh4w1n6fGqBbpToXk0CRLrvZDY6JpF8Pv+8TEi9cvD1cOB6pum9+zx7h28Hg8Kb6jsNeUv/aPFnXU/Ml1W6nV0Z6s21sa8POG4jymXjcxd60Jv98lDRmHbJst/tYPSN6IC6qMZHdwRTWTVrVLYKhJuXke/axw44/gBRPaImXylJlOPfHsPGmSpu1qnLG8wbV m00ooo@hp.lan",
+    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDt6YUa4DyWawKwiAdyrysZLuSgKupgsg4Nq9g4XoOi/bGw3OoTW1RJ6DOYSIVRWw9sdUgf7AG+f/fjX5DbA7JLKnKPpC/ZPJvjoQt5sc4U8d5LK4Oy6yEhXNpvVy+0okJvQyzQ5bAGM1GgmUFHkLcxE+skiWmE/WxZ04XuaTafgABIPX/LlamjCm/jGfBTaw/xWAt7C0IJIa0OQnD4COmDy0PZ4shNRqZl+XDNJqEg71MwAsoeegD7+S1VKW50oqxtWePwo6q4Wtw7vVPXbPGv+r6SYlnSqzQVnCcH+ElUb4eTJjZzDW1p3bSJI8l533b1jg+enlOiFEscmbjTl5jz m00ooo@legolas",
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINoqvo5NHbcilfU2/iiMQjK+crMHR0lrHH6MD0nvRXjy"
+};
+
